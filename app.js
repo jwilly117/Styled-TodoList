46d2ea603a2ed77d0f019dbb9ecd71bb660fe83c
jwilly117 <jakewilliams117@gmail.com>
2020-01-21 01:54:41 -0500

    Adding a few things

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 // hold the logic for storing todos and deleting todos
 
 alert("Source Loaded");
+
+var listCount = 0;
+
+$("add-to-do").on("click", function(event){
+    event.preventDefault();
+
+    // Get the to-do "value" from the textbox and store it in a variabl
+    var toDoTask = $("#to-do").val().trim();
+
+    // Create a new variable that will hold a <p> tag
+    // Then give it an ID in the folowing fomr;
+    // "item-4" or "item-3" or "item-99", where the number is equal to toDoCount
+    // Then set the to-do "value" as text to this <p> element.
+    var toDoItem = $("<p>");
+
+    toDoItem.attr
+
+
+});
